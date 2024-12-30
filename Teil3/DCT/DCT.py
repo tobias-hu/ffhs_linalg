1ce4c9f4ad6f209d68e0d25110d23888b68b7659
Töbu <tobias@helga.ch>
2024-12-30 21:02:20 +0100

    chore(part3): add images and first shot of python code

---
+++ Teil3/DCT/DCT.py
@@ -0,0 +1,28 @@
+import numpy as np
+import sympy as sp
+
+
+def fdct(N):
+    M = np.zeros((N, N))
+    for k in range(N):
+        for n in range(N):
+            M[k, n] = np.sqrt(1 / N) if k == 0 else np.sqrt(2/N) * sp.cos((sp.pi * (2 * n + 1) * k) / (2 * N))
+
+    return M
+
+
+def idct(N):
+    T = np.zeros((N, N))
+    for n in range(N):
+        for k in range(N):
+            T[n, k] = np.sqrt(1 / N) if k == 0 else np.sqrt(2/N) * sp.cos((sp.pi * (2 * n + 1) * k) / (2 * N))
+
+    return T
+
+
+def normalize(data):
+    return data-128
+
+def denormalize(data):
+    return data+128
+
---
+++ Teil3/DCT/main.py
@@ -0,0 +1,16 @@
+from DCT import fdct, idct, normalize, denormalize
+import numpy as np
+
+
+M = fdct(3)
+T = idct(3)
+
+img = normalize(np.array([255,128,45]))
+
+dct_res = M @ img
+
+print(dct_res)
+
+res = denormalize(np.round(T @ dct_res))
+
+print(res)
